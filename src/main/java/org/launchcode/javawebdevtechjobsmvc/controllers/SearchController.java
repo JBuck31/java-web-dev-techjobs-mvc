1d2b0e00182cd7a5a5d5981fb7188c8ae6f3c1de
JBuck31 <jrbuck31@gmail.com>
2019-12-30 09:56:18 -0600

    updated SearchController so that is returns search.html if the user enters "all" as the search term

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -1,6 +1,7 @@
 package org.launchcode.javawebdevtechjobsmvc.controllers;
 
 import org.launchcode.javawebdevtechjobsmvc.models.Job;
+import org.launchcode.javawebdevtechjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -26,14 +27,16 @@ public class SearchController {
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 //   ANY AND ALL OF THIS CAN BE CHANGED! Go back and read/re-watch the chapter!
     @RequestMapping(value = "results")
-    public ArrayList<Job> displaySearchResults(@RequestParam String searchType, String searchTerm, Model model) {
+    public String displaySearchResults(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+        ArrayList<Job> jobs;
         model.addAttribute("searchType", searchType);
         model.addAttribute("searchTerm", searchTerm);
 
         if (searchTerm.toLowerCase().equals("all")) {
-                return findAll();
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
             }
-        return null;
+        return "search";
     }
 
 }
